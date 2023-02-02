8f09f24f73323729af8e09f964cae44e41e3260b
mephit <meph.cell@gmail.com>
2023-02-03 00:19:45 +0300

    Add api-response sorting

--- api.py
+++ api.py
@@ -3,9 +3,18 @@ import json
 from recount_statistic import recount_statistic
 
 #response for /cats
-def get_cats_json(limit=None, offset=None):
+def get_cats_json(limit=None, offset=None, attribute=None, order=None): # TODO: need order by desc
+    sort_attrs = {'name': Cats.catname,
+                'color': Cats.catcolor,
+                'tail': Cats.tail,
+                'whiskers': Cats.whiskers,
+                }
     cats = []
-    for cat in Cats.select().limit(limit).offset(offset).dicts():
+    for cat in Cats.select(). \
+            limit(limit).     \
+            offset(offset).   \
+            order_by(sort_attrs.get(attribute, Cats.catname)). \
+            dicts():
         cats.append(cat)
     return cats
 
--- webserv.py
+++ webserv.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Request
 import uvicorn
+from typing import Union
 from orm_model import test_db_connection
 from recount_statistic import recount_statistic
 
@@ -12,20 +13,25 @@ def read_root():
     return "Cats Service. Version 0.1"
 
 @app.get("/cats")
-def give_cats(offset: int = None, limit: int = None):
-    return get_cats_json(offset=offset, limit=limit)
+def give_cats(offset: int = None,
+              limit: int = None,
+              attribute: Union[str, None] = None,
+              order: Union[str, None] = None
+              ):
+    return get_cats_json(offset=offset, limit=limit, attribute=attribute, order=order)
 
 @app.post("/cat")
 async def create_new_cat(a: Request):
     status = create_cat(await a.body())
     return {"status":status}
 
-@app.get("/recount") #debt: restrict frecuent requests
+@app.get("/recount") # TODO: need request limiter
 async def recount():
     return recount_statistic()
 
 if __name__ == "__main__":
     if test_db_connection():
         uvicorn.run("webserv:app", host="127.0.0.1", port=8080, log_level="info")
-    print('Configuration database connect was corrupted and will remove, restart application!')
-
+        recount_statistic()
+    else:
+        print('Configuration database connect was corrupted and will remove, restart application!')
